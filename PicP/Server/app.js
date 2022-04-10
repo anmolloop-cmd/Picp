cc71a949ca835463cf9ac1a3af965d13d18b9827
Rounak Agarwal <62253410+rounak29ag@users.noreply.github.com>
2022-04-10 13:48:45 +0530

    Update app.js

--- PicP/Server/app.js
+++ PicP/Server/app.js
@@ -3,7 +3,7 @@
     const router = require('./auth');
     const app=express();
     const port=3000;
-    const DB="mongodb+srv://admin:feO0J6tNYKgplSQr@cluster0.raqty.mongodb.net/PicP?retryWrites=true&w=majority"
+    const DB=PROCESS.ENV.MONGO_KEY;
     mongoose.connect(DB).then(()=>{console.log("connected")}).catch((err)=>console.log(err));
     app.use(require('./auth'));
     app.use(express.json());
