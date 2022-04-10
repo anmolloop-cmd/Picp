da72c2f8d69ee350356ae2f326c5497ae4437088
Rounak Agarwal <62253410+rounak29ag@users.noreply.github.com>
2022-04-10 13:43:02 +0530

    Mongo key removed

--- PicP/Server/app.js
+++ PicP/Server/app.js
@@ -3,7 +3,6 @@
     const router = require('./auth');
     const app=express();
     const port=3000;
-    const DB="mongodb+srv://admin:feO0J6tNYKgplSQr@cluster0.raqty.mongodb.net/PicP?retryWrites=true&w=majority"
     mongoose.connect(DB).then(()=>{console.log("connected")}).catch((err)=>console.log(err));
     app.use(require('./auth'));
     app.use(express.json());
